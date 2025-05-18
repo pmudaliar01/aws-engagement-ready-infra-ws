8a58d55d93666c9b7bf90edd4a1ef7d233df8936
pmudaliar01 <92157507+pmudaliar01@users.noreply.github.com>
2025-05-19 01:16:53 +0530

    Delete aws/website directory

--- aws/website/install_space_invaders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd git
-systemctl start httpd
-systemctl enable httpd
-cd /var/www/html
-git init
-git pull https://github.com/drehnstrom/space-invaders.git
--- aws/website/output.tf
+++
@@ -1,3 +0,0 @@
-output "load_balancer_dns" {
-  value = aws_elb.elb.dns_name
-}
